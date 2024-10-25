4db8d4f4d2c03827cdd7954bee8196b5f899991d
JoJoseph <sephtianyt@gmail.com>
2024-10-25 18:12:32 +0700

    style: add min width 250px

--- src/components/leaderboard/LeaderboardItem.jsx
+++ src/components/leaderboard/LeaderboardItem.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 
 function LeaderboardItem({ avatar, name, score }) {
   return (
-    <div className="space-y-3 border-b-2 border-gray-200 p-3 my-6 ">
-      <header className="flex justify-between">
+    <div className="min-w-[250px] space-y-3 border-b-2 border-gray-200 p-3 my-6 bg-white">
+      <header className="flex justify-between items-center">
         <div className="flex items-center gap-2 align-top">
           <img className="h-8 bg-cover aspect-square rounded-full" src={avatar} alt="user" />
-          <div className="font-medium mx-0 ">{name}</div>
+          <div className="font-medium mx-0">{name}</div>
         </div>
         <div className="text">{score}</div>
       </header>
